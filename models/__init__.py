b83c257754f88ae87fbae974c6b60f9c90192a88
Wang Yufeng <wangyf_1991@163.com>
2019-05-02 10:58:57 +0800

    Add files via upload

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+from .PSMnet import PSMNet
+from .WSMCnet import WSMCnet
+
+def model_by_name(name, maxdisp, C, S, rand_shift=False):
+    
+    if name.lower() == 'wsmcnet':
+        model = WSMCnet(maxdisp, C, S, rand_shift)
+    elif name.lower() == 'psmnet':
+        model = PSMNet(maxdisp)
+    else:
+        print('no model')
+    
+    return model
+
+
 
